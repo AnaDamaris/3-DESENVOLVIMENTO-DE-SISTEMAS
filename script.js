91fd5cdc323875d462093c1c87598de789f3308f
Ana.D <ana.ciriaco.soares@escola.pr.gov.br>
2026-07-08 09:18:17 -0300

    Add files via upload

--- script.js
+++
@@ -1,7 +0,0 @@
-const botao = document.getElementById("pedido");
-
-botao.addEventListener("click", () => {
-
-    alert("Obrigado pelo interesse! Em breve o sistema de pedidos estará disponível.");
-
-});
